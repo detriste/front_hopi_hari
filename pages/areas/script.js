26a940ac42ae0d7231908d77eefb3aa45e11cc73
detriste <detriste2018@gmail.com>
2025-05-27 10:53:15 -0300

    k

---
+++ pages/areas/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    const area = "Mistieri";
+    const token = localStorage.getItem('token');
+    const response = await fetch('/http://localhost:3000/brinquedos/${area}', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        authentication: {
+          
+        },
+
+        Authorization: `Bearer` + token
+    });
+
+    const data = await response.json();
+
+})
